4b79702d74df57a71d344533c0245635a2eb8108
Robin <almigod0718@gmail.com>
2017-05-09 17:54:46 +0800

    loggin update

--- src/hbs/hbsBuild.js
+++ src/hbs/hbsBuild.js
@@ -3,13 +3,15 @@ const fs = require('fs')
 const path = require('path')
 
 const srcFileName = 'hbs-index.html'
-const buildFileName = 'hbs-index.html'
 const srcFilePath = path.join(__dirname, srcFileName)
+const buildFileName = 'hbs-index.html'
 const buildDir = path.join(__dirname, 'build')
+const buildFilePath = path.join(buildDir, buildFileName)
 
 const log = (...args) => console.log('  ->', args.join(' '))
 
-log('Reading html from', srcFilePath)
+log('Start Building hbs template')
+log('Reading html')
 
 const rawHtml = fs.readFileSync(srcFilePath, 'utf8')
 
@@ -38,12 +40,14 @@ try {
 </html>
 `
 
-  if (!fs.existsSync(buildDir)){
+  if (!fs.existsSync(buildDir)) {
     fs.mkdirSync(buildDir)
   }
 
-  fs.writeFileSync(path.join(__dirname, 'build', buildFileName), content,'utf8')
+  fs.writeFileSync(path.join(__dirname, 'build', buildFileName), content, 'utf8')
 
+  log('Input File  :', srcFilePath)
+  log('Output File :', buildFilePath)
   log('Done')
 
 } catch (err) {
