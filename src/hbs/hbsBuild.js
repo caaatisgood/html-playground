b8d640f9ee0ce3cb6fed524c4f770e1ad9b864b4
Robin <almigod0718@gmail.com>
2017-11-29 03:14:38 +0800

    Optimise hbs building script

--- src/hbs/hbsBuild.js
+++ src/hbs/hbsBuild.js
@@ -17,34 +17,22 @@ const rawHtml = fs.readFileSync(srcFilePath, 'utf8')
 
 const { document } = (new JSDOM(rawHtml)).window
 
-const templateDOM = document.querySelector('#hbsTemplate')
+const rootWrap = document.getElementById('hbsTemplate')
 
-if (!templateDOM) throw new Error('hbs template not found. You should add an id "hbsTemplate" to the hbs script tag')
+rootWrap.outerHTML = rootWrap.innerHTML
 
-const rawTemplate = templateDOM.innerHTML
+document.getElementById('hbsLib').remove()
+document.getElementById('hbsScript').remove()
 
 try {
 
   log('Building hbs raw template')
 
-  const content =
-`<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>hbs template</title>
-</head>
-<body>
-  ${rawTemplate}
-</body>
-</html>
-`
-
   if (!fs.existsSync(buildDir)) {
     fs.mkdirSync(buildDir)
   }
 
-  fs.writeFileSync(path.join(__dirname, 'build', buildFileName), content, 'utf8')
+  fs.writeFileSync(path.join(__dirname, 'build', buildFileName), document.documentElement.outerHTML, 'utf8')
 
   log('Input File  :', srcFilePath)
   log('Output File :', buildFilePath)
